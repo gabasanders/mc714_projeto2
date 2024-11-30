3ed7493d07a5ac1740a75b8ee5b712f548a59e9d
Gabriel <gabrielsps03@gmail.com>
2024-11-29 21:04:27 -0300

    implementa TokenRing, RPC e Lamport

--- Game.py
+++ Game.py
@@ -1,25 +1,87 @@
-from TokenRing import TokenRing
-from Process import ProcessClass
+from Process import Player
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+from socketserver import ThreadingMixIn
+import threading
 
-def makeMove(process):
+# Servidor com suporte a múltiplas threads
+class ThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    pass
 
-    if process.token == 1:
-        print("Success!")
-        tr.passToken(process.id)
+# Inicia o servidor de um jogador
+def init_server(player, host, port):
+    server = ThreadedXMLRPCServer((host, port), allow_none=True)
+    server.register_instance(player)
+    print(f"Servidor do Jogador {player.id} rodando em {host}:{port}")
+    server.serve_forever()
+
+
+
+def auto():
+
+    addr =["http://localhost:8000","http://localhost:8001","http://localhost:8002","http://localhost:8003"]
+
+    # Adiciona jogadores
+    p1 = Player(1, addr= addr[0], next_address=addr[1], all_addr=addr, announce= True,auto=True)
+    p2 = Player(2, addr= addr[1], next_address=addr[2], all_addr=addr, announce= True,auto=True)
+    p3 = Player(3, addr= addr[2], next_address=addr[3], all_addr=addr, announce= True,auto=True)
+    p4 = Player(4, addr= addr[3], next_address=addr[0], all_addr=addr, announce= True,auto=True)
+
+    players = [p1,p2,p3,p4]
+
+    # Iniciar servidores RPC em threads separadas
+    t1 = threading.Thread(target=init_server, args=(p1, "localhost", 8000))
+    t2 = threading.Thread(target=init_server, args=(p2, "localhost", 8001))
+    t3 = threading.Thread(target=init_server, args=(p3, "localhost", 8002))
+    t4 = threading.Thread(target=init_server, args=(p4, "localhost", 8003)) 
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+
+    # Passar o token para o primeiro jogador
+    with xmlrpc.client.ServerProxy("http://localhost:8000") as proxy:
+        proxy.receive_token()
+
+def manual():
+    
+    addr =["http://localhost:8000","http://localhost:8001","http://localhost:8002","http://localhost:8003"]
+
+    # Adiciona jogadores
+    p1 = Player(1, addr= addr[0], next_address=addr[1], all_addr=addr, announce= True)
+    p2 = Player(2, addr= addr[1], next_address=addr[2], all_addr=addr, announce= True)
+    p3 = Player(3, addr= addr[2], next_address=addr[3], all_addr=addr, announce= True)
+    p4 = Player(4, addr= addr[3], next_address=addr[0], all_addr=addr, announce= True)
+
+    players = [p1,p2,p3,p4]
+
+    # Iniciar servidores RPC em threads separadas
+    t1 = threading.Thread(target=init_server, args=(p1, "localhost", 8000))
+    t2 = threading.Thread(target=init_server, args=(p2, "localhost", 8001))
+    t3 = threading.Thread(target=init_server, args=(p3, "localhost", 8002))
+    t4 = threading.Thread(target=init_server, args=(p4, "localhost", 8003)) 
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+
+    # Passar o token para o primeiro jogador
+    with xmlrpc.client.ServerProxy("http://localhost:8000") as proxy:
+        proxy.receive_token()
+
+    while True:
+        entrada = int(input())
+        players[entrada].make_move()
+
+if __name__ == "__main__":
+
+    print("Defina o modo que deseja executar: Automático (1) ou Manual? (2)")
+    modo = input()
+
+    if modo == '1':
+        auto()
     else:
-        print("Wait for your turn!")
-        
-
-p0 = ProcessClass(0)
-p1 = ProcessClass(1)
-processes = [p0,p1]
-
-tr = TokenRing(processes)
-
-
-makeMove(p1)
-makeMove(p0)
-makeMove(p0)
-makeMove(p1)
-
+        manual()
 
--- Process.py
+++ Process.py
@@ -1,5 +1,59 @@
-class ProcessClass:
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+import threading
+import time
 
-    def __init__(self,id) -> None:
-        self.id = id
-        self.token = 0
+class Player:
+    def __init__(self, id, addr, next_address, all_addr, auto = False, announce = False):
+        self.id = id                        #Identificador
+        self.token = False                  # Token 
+        self.next_address = next_address    # Endereço do próximo processo
+        self.announce = announce            # Determina se irá printar movimentos do token
+        self.leader = False                 # Indica se é o líder
+        self.clock = 0                      # Contador do processo
+        self.all_addr = all_addr            # Lista de todos os endereços
+        self.addr = addr                    # Endereço do processo
+        self.auto = auto                    # Determina se irá fazer a jogada automaticamente
+
+    # =============================  Token Ring  =================================
+
+    def receive_token(self): # Recebe o token 
+        self.token = True
+        if self.announce:
+            print(f"Jogador {self.id} recebeu o token.")
+        
+        if self.auto:
+
+            self.make_move()
+
+    def make_move(self): # Tenta fazer uma jogada
+        if self.token:
+            print(f"Jogador {self.id} realizou sua jogada.")
+            self.update_clock()
+            self.send_event()
+            self.token = False
+            self.pass_token()
+        else:
+            print(f"Espere por sua vez, Jogador {self.id}!")
+
+    def pass_token(self):
+        with xmlrpc.client.ServerProxy(self.next_address) as proxy:
+            if self.announce:
+                print(f"Jogador {self.id} passando o token.")
+                print("")
+            proxy.receive_token()
+
+    # =============================  Lamport  =================================
+
+    def update_clock(self):
+        self.clock += 1
+
+    def receive_event(self,sender_id,received_clock):
+        self.clock = max(self.clock,received_clock) + 1
+        print(f"Jogador {self.id} recebeu o evento do jogador {sender_id}")
+
+    def send_event(self):
+        for addr in self.all_addr:
+            if addr != self.addr:
+                with xmlrpc.client.ServerProxy(addr) as proxy:
+                    proxy.receive_event(self.id, self.clock)
